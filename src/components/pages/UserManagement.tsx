1f1f5ff7866f5ec6626f9150d01b3b88678fe3b5
Yoshitaka Kato <83825593+Yoshitaka1999@users.noreply.github.com>
2022-05-01 07:55:53 +0900

    add User management page

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,16 @@
+import { Box, Image, Stack, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  return (
+    <Wrap>
+      <WrapItem>
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+          <Stack textAlign="center">
+            <Image src="" />
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
